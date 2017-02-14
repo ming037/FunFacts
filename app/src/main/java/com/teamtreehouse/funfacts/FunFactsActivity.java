b7172de33d9b86e633cedf0170bf120ce59e60c4
HAN HWI <ming037@naver.com>
2017-02-15 01:34:36 +0900

    onRestoreInstanceState //인스턴스를 불러오는 함수

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -1,18 +1,28 @@
 package com.teamtreehouse.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import static com.teamtreehouse.funfacts.R.id.relativeLayout;
+import static com.teamtreehouse.funfacts.R.id.showFactButton;
+
 
 public class FunFactsActivity extends Activity {
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     private static final String KEY_FACT = "KEY_FACT";
     private static final String KEY_COLOR = "KEY_COLOR";
-    private String mFact;
-    private int mColor;
+    private String mFact = mFactBook.mFacts[0];
+    private int mColor = Color.parseColor(mColorWheel.mColors[8]);
+
+    private TextView mfactLabel;
+    private Button mshowFactButton;
+    private RelativeLayout mrelativeLayout;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -22,8 +32,17 @@ public class FunFactsActivity extends Activity {
         outState.putInt(KEY_COLOR, mColor);
     }
 
-    private FactBook mFactBook = new FactBook();
-    private ColorWheel mColorWheel = new ColorWheel();
+    @Override //onCreate에서 해도 되지만 그때는 NULL일 때를 신경써줘야 함.
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mFact = savedInstanceState.getString(KEY_FACT);
+        mColor = savedInstanceState.getInt(KEY_COLOR);
+
+        mfactLabel.setText(mFact);
+        mrelativeLayout.setBackgroundColor(mColor);
+        mshowFactButton.setTextColor(mColor);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +50,21 @@ public class FunFactsActivity extends Activity {
         setContentView(R.layout.activity_fun_facts);
 
         // Declare our View variables and assign them the Views from the layout file
-        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
-        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        mfactLabel = (TextView) findViewById(R.id.factTextView);
+        mshowFactButton = (Button) findViewById(showFactButton);
+        mrelativeLayout = (RelativeLayout) findViewById(relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mFact = mFactBook.getFact();
-                factLabel.setText(mFact);
+                mfactLabel.setText(mFact);
 
                 mColor = mColorWheel.getColor();
-                relativeLayout.setBackgroundColor(mColor);
-                showFactButton.setTextColor(mColor);
+                mrelativeLayout.setBackgroundColor(mColor);
+                mshowFactButton.setTextColor(mColor);
             }
         };
-        showFactButton.setOnClickListener(listener);
+        mshowFactButton.setOnClickListener(listener);
     }
 }
